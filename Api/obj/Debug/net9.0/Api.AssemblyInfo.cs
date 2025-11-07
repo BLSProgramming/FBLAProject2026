157cc845b880608dbeea91a3e83e5a988c9238bf
BLSProgramming <bls2.working@gmail.com>
2025-11-07 09:53:33 -0500

    S

--- Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f90329be5f491ef674d7ecd6a336d795f0ab3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be930ad7c333f21a636ceabc9aeb8d022bf8209")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
